6c18fc6da40ba2241805a2de0d14a1af6120533b
petushka1 <nadya@devery.io>
2022-09-06 23:59:46 +0300

    setup repo and create classes

---
+++ src/person.rb
@@ -0,0 +1,71 @@
+class Person
+  def initialize(age, name = "Unknown", parent_permission = true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  # getters
+
+  def id
+    @id
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  def parent_permission
+    @parent_permission
+  end
+
+  # setters
+
+  def name=(value)
+    @name = value
+  end
+
+  def age=(value)
+    @age = value
+  end
+
+  def can_use_services?
+    if self.is_of_age? || @parent_permission == true
+      return true
+    else
+      return false
+    end
+  end
+
+  private
+
+  def is_of_age?
+    if @age > 18
+      return true
+    else
+      return false
+    end
+  end
+end
+
+# test cases
+
+# person1 = Person.new(38, "Nick")
+# person1
+# puts person1.age
+# puts person1.parent_permission
+
+# puts person1.can_use_services?
+
+# person2 = Person.new(16, "Mark", false)
+# person2
+# puts person2.can_use_services?
+
+# person3 = Person.new(10, "Maggy")
+# person3
+# puts person3.can_use_services?
---
+++ src/teacher.rb
@@ -0,0 +1,19 @@
+require './person.rb'
+
+class Teacher < Person
+  def initialize(specialization, age, name = "Unknown")
+    super(age, name)
+    @specialization = specialization
+  end
+
+  def can_use_services?
+    return true
+  end
+end
+
+# test cases
+
+# teacher1 = Teacher.new('Math', 38, "Kate Williams")
+# teacher1
+# puts teacher1.name
+# puts teacher1.can_use_services?
