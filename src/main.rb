fb54315319d9b0c219c5b34dcc4d5d2a4f6f9013
petushka1 <nadya@devery.io>
2022-09-13 13:36:35 +0300

    add refactored solution

--- src/main.rb
+++ src/main.rb
@@ -1,92 +1,84 @@
-#!/usr/bin/env ruby
 require_relative 'prosessor'
 
-  def greeting
-    commands = [
-      '[1] List all books',
-      '[2] List all people',
-      '[3] Create person',
-      '[4] Create book',
-      '[5] Create rental',
-      '[6] List rentals by person',
-      '[7] Quit'
-    ]
-    commands.each { |cmd| puts cmd }
-    puts 'Make your choice to continue...'
-  end
+def greeting
+  commands = [
+    '[1] List all books',
+    '[2] List all people',
+    '[3] Create person',
+    '[4] Create book',
+    '[5] Create rental',
+    '[6] List rentals by person',
+    '[7] Quit'
+  ]
+  commands.each { |cmd| puts cmd }
+  puts 'Make your choice to continue...'
+end
 
-  def user_choice(choice, app) # rubocop:disable Metrics/CyclomaticComplexity
-    case choice
-    when '1'
-      app.list_all_books
-    when '2'
-      app.list_all_people
-    when '3'
-      create_person(app)
-    when '4'
-      app.create_book
-    when '5'
-      app.create_rental
-    when '6'
-      app.list_rentals_by_id
-    when '7'
-      return true
-    else
-      'Believe in your self! Press the right key!'
-    end
-    false
+def user_choice(choice, app) # rubocop:disable Metrics/CyclomaticComplexity
+  case choice
+  when '1'
+    app.list_all_books
+  when '2'
+    app.list_all_people
+  when '3'
+    create_person(app)
+  when '4'
+    app.create_book
+  when '5'
+    app.create_rental
+  when '6'
+    list_rentals_by_id(app)
+  when '7'
+    return true
+  else
+    'Believe in your self! Press the right key!'
   end
+  false
+end
 
-  def create_person(app)
-    puts "To create a [Student] press [1]\nTo create a [Teacher] press [2]"
-    choice = gets.chomp
-    case choice
-    when '1'
-      app.create_student
-    when '2'
-      app.create_teacher
-    else
-      'Select person category first'
-    end
+def create_person(app)
+  puts "To create a [Student] press [1]\nTo create a [Teacher] press [2]"
+  choice = gets.chomp
+  case choice
+  when '1'
+    app.create_student
+  when '2'
+    app.create_teacher
+  else
+    'Select person category first'
   end
+end
 
-  def check_permission(age)
-    if age.to_i < 18
-      puts 'Has the student got a parent permission? [Y/N]'
-      input = gets.chomp
-      case input
-      when 'y' || 'Y'
-        true
-      when 'n' || 'N'
-        false
-      end
-    else
-      true
-    end
-  end
-
-=begin
-  def list_rentals_by_id(app)
-    puts 'Enter person ID to see her/his rentals or press [2] to get a list of people'
+def check_permission(age)
+  if age.to_i < 18
+    puts 'Has the student got a parent permission? [Y/N]'
     input = gets.chomp
     case input
-    when '2'
-      app.list_all_people
-    else
-      app.list_rentals(input.to_i)
+    when 'y' || 'Y'
+      true
+    when 'n' || 'N'
+      false
     end
+  else
+    true
   end
-=end
+end
 
-  def main
-    app = Prosessor.new
-    exit = false
-    puts 'Welcome to Ruby Hell!'
-    while exit == false
-      greeting
-      exit = user_choice(gets.chomp, app)
-    end
-    puts 'Ruby will find you..'
+def list_rentals_by_id(app)
+  puts 'Enter person ID to see her/his rentals or press [2] to get a list of people'
+  input = gets.chomp
+  app.list_all_rentals_by_id(input)
+end
+
+def main
+  app = Prosessor.new
+  exit = false
+  puts 'Welcome to Ruby Hell!'
+  while exit == false
+    greeting
+    exit = user_choice(gets.chomp, app)
   end
+  puts 'Ruby will find you..'
+end
 
 main
--- src/prosessor.rb
+++ src/prosessor.rb
@@ -1,90 +1,96 @@
 require_relative 'app'
 
 class Prosessor < App
-    attr_reader :classrooms, :students, :teachers, :books, :rentals
+  attr_reader :classrooms, :students, :teachers, :books, :rentals
 
-   def create_student
-        puts 'Enter Student [Name]'
-        name = gets.chomp
-        puts 'Enter Student [Age]'
-        age = gets.chomp
-        parent_permission = check_permission(age)
-        _create_student(age, parent_permission, name)
-        puts "New Student added:\n[Student] ID: #{students.last.id}, "
-        print "Name: #{students.last.name}, Age: #{students.last.age} "
-        print "Parent permission: #{students.last.parent_permission ? 'YES' : 'NO'}\n"
-    end
+  def create_student
+    puts 'Enter Student [Name]'
+    name = gets.chomp
+    puts 'Enter Student [Age]'
+    age = gets.chomp
+    parent_permission = check_permission(age)
+    _create_student(age, parent_permission, name)
+    puts "New Student added:\n[Student] ID: #{students.last.id}, "
+    print "Name: #{students.last.name}, Age: #{students.last.age} "
+    print "Parent permission: #{students.last.parent_permission ? 'YES' : 'NO'}\n"
+  end
 
-    def create_teacher
-        puts 'Enter Teacher [Name]'
-        name = gets.chomp
-        puts 'Enter Teacher [Age]'
-        age = gets.chomp
-        puts 'Enter Teacher [Specialization]'
-        specialization = gets.chomp
-        _create_teacher(specialization, age, name)
-        puts "New Teacher added:\n[Teacher] ID: #{teachers.last.id}, "
-        print "Name: #{teachers.last.name}, Age: #{teachers.last.age}, "
-        print "Specialization: #{teachers.last.specialization}\n"
-    end
+  def create_teacher
+    puts 'Enter Teacher [Name]'
+    name = gets.chomp
+    puts 'Enter Teacher [Age]'
+    age = gets.chomp
+    puts 'Enter Teacher [Specialization]'
+    specialization = gets.chomp
+    _create_teacher(specialization, age, name)
+    puts "New Teacher added:\n[Teacher] ID: #{teachers.last.id}, "
+    print "Name: #{teachers.last.name}, Age: #{teachers.last.age}, "
+    print "Specialization: #{teachers.last.specialization}\n"
+  end
 
-    def create_book
-        puts 'Read nothing? Just google some [Book] to add!'
-        puts 'Enter Book [Title]'
-        title = gets.chomp
-        puts 'Enter Book [Author]'
-        author = gets.chomp
-        _create_book(title, author)
-        puts "New Book added:\n[Book] Title: #{books.last.title}, Author: #{books.last.author}\n"
-    end
+  def create_book
+    puts 'Read nothing? Just google some [Book] to add!'
+    puts 'Enter Book [Title]'
+    title = gets.chomp
+    puts 'Enter Book [Author]'
+    author = gets.chomp
+    _create_book(title, author)
+    puts "New Book added:\n[Book] Title: #{books.last.title}, Author: #{books.last.author}\n"
+  end
 
-    def create_rental
-        msg = 'There should be at least one person and one book'
-        return puts msg unless students.length.positive? || (teachers.length.positive? && books.length.positive?)
-    
-        book = select_book
-        person = select_person
-        _create_rental(book, person)
-    
-        puts 'New Rental added:'
-        puts "[Rental] Date: #{rentals.last.date}"
-        puts "---------Book: #{rentals.last.book.title}"
-        puts "---------Person: #{rentals.last.person.name}\n"
+  def create_rental
+    msg = 'There should be at least one person and one book'
+    return puts msg unless students.length.positive? || (teachers.length.positive? && books.length.positive?)
+
+    book = select_book
+    person = select_person
+    _create_rental(book, person)
+
+    puts 'New Rental added:'
+    puts "[Rental] Date: #{rentals.last.date}"
+    puts "---------Book: #{rentals.last.book.title}"
+    puts "---------Person: #{rentals.last.person.name}\n"
+  end
+
+  def check_input(input, max)
+    input.between?(0, max)
+  end
+
+  def select_book
+    puts 'Select a book from the list'
+    list_all_books
+    check = false
+    while check == false
+      puts 'Enter a Book number'
+      input = gets.chomp.to_i
+      check = check_input(input, books.length - 1)
     end
-    
-    def check_input(input, max)
-        input.between?(0, max)
+    books[input]
+  end
+
+  def select_person
+    puts 'Select a person from the list'
+    list_all_people
+    check = false
+    while check == false
+      puts 'Enter a Person number'
+      input = gets.chomp.to_i
+      max = students.length + (teachers.length - 1)
+      check = check_input(input, max)
     end
-    
-    def select_book
-        puts 'Select a book from the list'
-        list_all_books
-        check = false
-        while check == false
-        puts 'Enter a Book number'
-        input = gets.chomp.to_i
-        check = check_input(input, books.length - 1)
-        end
-        books[input]
+    if input >= students.length
+      teachers[input - students.length]
+    else
+      students[input]
     end
-    
-    def select_person
-        puts 'Select a person from the list'
-        list_all_people
-        check = false
-        while check == false
-        puts 'Enter a Person number'
-        input = gets.chomp.to_i
-        max = students.length + (teachers.length - 1)
-        check = check_input(input, max)
-        end
-        if input >= students.length
-        teachers[input - students.length]
-        else
-        students[input]
-        end
+  end
+
+  def list_all_rentals_by_id(input)
+    case input
+    when '2'
+      list_all_people
+    else
+      list_rentals(input.to_i)
     end
+  end
 end
-
-  
-  
